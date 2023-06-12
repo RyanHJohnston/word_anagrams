ed0361a10600aea149bdcf381267d867325add5c
Ryan H. Johnston <ryanhjohnstoncollege@gmail.com>
2023-06-12 03:37:46 -0400

    Only have task 5 left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import zipfile
 import collections
-import math
+from itertools import permutations
 
 # Task 1: unzip words.zip and ls output
 zipfile.ZipFile('words.zip').extractall('.')
@@ -10,17 +10,14 @@ file = open("words/words.txt", "r");
 
 wordlist = []
 
-with open("words/words.txt", "r") as file:
-    wordlist = file.readlines() # all data in words.txt are stored in read_data
-file.close()
+wordlist = file.readlines() # all data in words.txt are stored in read_data
 
 # Task 2:
 # print(wordlist[:10])
 # print(len(wordlist))
 
 # converting a list to a set automatically removes the duplicates
-# wordclean = sorted(list(set(str(file).strip().lower() for file in wordlist)))
-# print(wordclean[:10])
+wordclean = sorted(list(set(str(file).strip().lower() for file in wordlist)))
 
 # Task 3
 """
@@ -31,4 +28,69 @@ word length
 """
 
 # dictionary called words_bylength
+item = 0
+words_bylength = {0 : []}
 
+# index is the index (duh) and value is the string
+# words_bylength[key] = value
+def create_length_dictionary(strings):
+    length_dict = {}
+
+    for string in strings:
+        length = len(string)
+        if length in length_dict:
+            length_dict[length].append(string)
+        else:
+            length_dict[length] = [string]
+
+    return length_dict
+
+result_dict = create_length_dictionary(wordclean)
+
+# Print the resulting dictionary
+# for length, strings in result_dict.items():
+    #    print(f"Strings of length {length}: {strings}")
+
+# print(result_dict)
+
+
+# Task 4:
+def generate_anagrams(word):
+    # Generate all permutations of the letters in the word
+    perms = permutations(word)
+    
+    # Join each permutation to form a string and store it in a list
+    anagrams = [''.join(perm) for perm in perms]
+    
+    return anagrams
+
+def create_anagram_dict(words):
+    anagram_dict = {}
+    
+    for word in words:
+        # Calculate the length of the word
+        length = len(word)
+        
+        # If length key doesn't exist in the outer dictionary, create it with an empty inner dictionary
+        if length not in anagram_dict:
+            anagram_dict[length] = {}
+        
+        # If word key doesn't exist in the inner dictionary, generate anagrams and store them as a list
+        if word not in anagram_dict[length]:
+            anagram_list = generate_anagrams(word)
+            anagram_dict[length][word] = anagram_list
+    
+    return anagram_dict
+
+result_dict = create_anagram_dict(wordclean)
+
+for length, inner_dict in result_dict.items():
+    print(f"Length: {length}")
+    for word, anagrams in inner_dict.items():
+        print(f"{word}: {anagrams}")
+    print()
+
+# Task 5:
+
+
+file.close()
